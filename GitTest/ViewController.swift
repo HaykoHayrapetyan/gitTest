1b7132ed511be29e7ebf9b130b02f2b03f0aba1f
HaykoHayrapetyan <h23a05y93k@gmail.com>
2020-04-28 19:04:38 +0400

    ...

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // changed in master
+        
+        
+        
+        
     }
 
 
